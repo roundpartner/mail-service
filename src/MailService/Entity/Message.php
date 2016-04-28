978fd1bd3b1e9fd07ce0bfba305def5a1109e603
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-04-28 01:04:32 +0100

    Add variables to entity

--- src/MailService/Entity/Message.php
+++ src/MailService/Entity/Message.php
@@ -4,4 +4,23 @@ namespace MailService\Entity;
 
 class Message
 {
+    /**
+     * @var string
+     */
+    public $from;
+
+    /**
+     * @var string
+     */
+    public $to;
+
+    /**
+     * @var string
+     */
+    public $subject;
+
+    /**
+     * @var string
+     */
+    public $text;
 }
